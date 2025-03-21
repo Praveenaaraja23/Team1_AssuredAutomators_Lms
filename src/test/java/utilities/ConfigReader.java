d0b0511a68ec36bef189e0accb1811cd27b98795
Praveenaaraja23 <praveenaaraja23@gmail.com>
2025-03-21 00:53:04 -0400

    Intial commit

---
+++ src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,44 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.io.InputStream;
+public class ConfigReader {
+	private static final Properties properties = new Properties();
+    private static final String CONFIG_FILE_PATH = "src/test/resources/config.properties";
+    static {
+        loadConfigProperties();
+    }
+    public static void loadConfigProperties() {
+        try (InputStream inputStream = new FileInputStream(CONFIG_FILE_PATH)) {
+            properties.load(inputStream);
+            System.out.println("Config File Loaded: " + CONFIG_FILE_PATH);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+    public static String getUserId() {
+        return properties.getProperty("userId");
+    }
+    public static String getUserFirstName() {
+        return properties.getProperty("userFirstName");
+    }
+    // updating user id and username dynamically
+    public static void updateUserDetailsInConfig(int userId, String userFirstName) {
+        try {
+            properties.setProperty("userId", String.valueOf(userId));
+            properties.setProperty("userFirstName", userFirstName);
+            try (FileOutputStream outputStream = new FileOutputStream(CONFIG_FILE_PATH)) {
+                properties.store(outputStream, "Updated user details dynamically");
+            }
+            System.out.println("Updated userId and userFirstName in config.properties");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to update config.properties", e);
+        }
+    }
+	}
