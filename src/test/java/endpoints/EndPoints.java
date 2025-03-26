bc9f40333995aa76b0daa486243f57748412845c
VD136 <vanid373@gmail.com>
2025-03-26 14:02:32 -0400

    Class module part two test case with necessary dependencies -Vani

--- src/test/java/endpoints/EndPoints.java
+++ src/test/java/endpoints/EndPoints.java
@@ -14,8 +14,22 @@ public enum EndPoints {
     DELETE_BATCH_ID("/batches/{id}"),
 	
 	//user endpoint
-	CREATE_USER("/users/roleStatus");
+	CREATE_USER("/users/roleStatus"),
     
+	//Class Module part 2 Endpoints
+	 Get_AllClass_byStaffID("/classesByStaff/{staffId}"),
+	    Get_AllClass_byStaffID_Invalid("/classesByStaff/staffId/http"),
+	    Get_ALlRecordings("/classrecordings"),
+	     Get_ALlRecordings_Invalid("/classrecordings/staff"),
+	   Get_AllRecordings_byClassID("/classRecordings/{classId}"),
+	  Get_AllRecordings_byClassID_Invalid("/classRecordings/cl"),
+	    Put_Class("/updateClass/{classId}"),
+	    Put_Class_Invalid("/updateClass/lassId"),
+	     Put_classREcording("/updateClassrecording/{classId}"),
+	  Put_classREcordingInvalidEnd ("/updateClassrecording/classId"),
+	    Delete_classByID("/deleteByClass/{classId}"),
+	     Delete_classByID_Invalid("/deleteByClass/classId");
+	    //***class part 2 code END
     private final String endpoint;
 
     EndPoints(String endpoint) {
